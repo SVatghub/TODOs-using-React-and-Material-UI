df493024e6b06e12d8012bcdd6acb04ac3d5fd4c
sahil <sahil0000002@gmail.com>
2024-09-11 23:26:43 +0530

    Adding Local Storage and changing the items to objects with a unique id and completed status

--- src/components/todoForm/TodoForm.jsx
+++ src/components/todoForm/TodoForm.jsx
@@ -1,31 +1,52 @@
 import { Button } from "@mui/material";
 import TodoItem from "../todoItem/TodoItem";
 import styles from "./TodoForm.module.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import EditIcon from "@mui/icons-material/Edit";
 import InputAdornment from "@mui/material/InputAdornment";
 import { FilledInput } from "@mui/material";
 import { InputLabel } from "@mui/material";
 import { FormControl } from "@mui/material";
+import { v4 as uuidv4 } from "uuid";
+
+// we can also use crypto.randomUUID() to get random id
+
+const loadData = () => {
+  const data = JSON.parse(localStorage.getItem("todos"));
+  if (!data) return [];
+  return data;
+};
 
 export default function TodoForm() {
   const [newItem, setNewItem] = useState("");
-  const [itemsLabels, setItemLabels] = useState(["HomeWork", "Code"]);
+  const [itemsLabels, setItemLabels] = useState(loadData());
 
-  const handleIsCompleted = () => {};
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(itemsLabels));
+  }, [itemsLabels]);
 
-  const handleIsRemoved = (index) => {
+  const handleIsCompleted = (id) => {
     setItemLabels((prevItemLabels) => {
-      return (prevItemLabels = prevItemLabels.filter(
-        (_, idx) => idx !== index
-      ));
+      return prevItemLabels.map((item) => {
+        if (item.id === id) return { ...item, completed: !item.completed };
+        else return item;
+      });
+    });
+  };
+
+  const handleIsRemoved = (id) => {
+    setItemLabels((prevItemLabels) => {
+      return (prevItemLabels = prevItemLabels.filter((item) => item.id !== id));
     });
   };
 
   const handleAddItem = () => {
     if (newItem.trim()) {
       setItemLabels((prevItemLabels) => {
-        return (prevItemLabels = [...prevItemLabels, newItem]);
+        return (prevItemLabels = [
+          ...prevItemLabels,
+          { text: newItem, id: uuidv4(), completed: false },
+        ]);
       });
       setNewItem("");
     }
@@ -36,13 +57,13 @@ export default function TodoForm() {
       <h1 className={styles.title}>Todos</h1>
       <div className={styles.todoFormContainer}>
         <div className="items-container">
-          {itemsLabels.map((i, idx) => {
+          {itemsLabels.map((i) => {
             return (
               <TodoItem
-                key={idx}
-                item={i}
-                handleIsCompleted={handleIsCompleted}
-                handleIsRemoved={() => handleIsRemoved(idx)}
+                key={i.id}
+                item={i.text}
+                handleIsCompleted={() => handleIsCompleted(i.id)}
+                handleIsRemoved={() => handleIsRemoved(i.id)}
               />
             );
           })}
